81b59b588f95438a000b0fbdd5e76cce482393f1
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-05-09 10:49:14 +0000

    S10: Use common stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,8 @@ TARGET_COPY_OUT_VENDOR := vendor
 # A/B
 AB_OTA_UPDATER := true
 TW_INCLUDE_REPACKTOOLS := true
+USE_COMMON_BOOTCTRL := true
+USE_COMMON_GPTUTILS := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 buildvariant=user veritykeyid=id:7e4333f9bba00adfe0ede979e28ed1920492b40f
